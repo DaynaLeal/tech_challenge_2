a114f1634c56202416701aba1951e2b85a09c139
Dayna Leal <dayna.r.leal@gmail.com>
2020-09-14 00:39:16 -0500

    cleared port 5000 errors

--- react-app/server/index.js
+++ react-app/server/index.js
@@ -1,17 +1,28 @@
+import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import songsRoutes from './routes/songs.js';
 
+
 const app = express();
 const PORT = 5000;
+var __dirname = path.resolve();
+
+
+// middlewares
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
 
 app.use(bodyParser.json());
 
 app.use('/songs', songsRoutes);
 
 app.get('/', (req, res) => {
-  console.log(data);
-  res.send('Welcome to My Homepage');
+  res.sendFile('public/index.html' , { root : __dirname});
+});
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT}`));
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import './style.css';
 
-function App() {
+export default class App extends React.Component{
+  state = {
+    songs: [],
+  };
+  componentDidMount() {
+    axios.get("./data/songs.json").then((response) => {
+      this.setState({songs: response.data});
+    });
+  }
+  render() {
+  const { songs } = this.state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul className="songs">
+          {songs.map((song) => (
+            <li className="song">
+              <p>
+                <strong>Title:</strong> {song.title}
+              </p>
+              <p>
+                <strong>Artist:</strong> {song.artist}
+              </p>
+              <p>
+                <strong>Year:</strong> {song.year}
+              </p>
+            </li>
+          ))}
+        </ul>
     </div>
   );
+  }
 }
 
-export default App;
