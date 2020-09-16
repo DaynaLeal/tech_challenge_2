9d1cfc9cff930af77a3860a2b160f075571f48de
Dayna Leal <dayna.r.leal@gmail.com>
2020-09-16 00:33:16 -0500

    fixed server/index.js, public/index.html saving properly again

--- react-app/server/index.js
+++ react-app/server/index.js
@@ -1,27 +1,22 @@
-import path from 'path';
 import express from 'express';
-import bodyParser from 'body-parser';
-import songsRoutes from './routes/songs.js';
+import path from 'path';
 
+import songsRoutes from './routes/songs.js';
 
 const app = express();
 const PORT = 5000;
+
 var __dirname = path.resolve();
+var rootDirecotry = path.join(__dirname, "..");
 
-// middlewares
-app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static("public"));
 
-app.use(bodyParser.json());
+app.get('/', function(req, res){
+  res.sendFile('/public/index.html', { root: rootDirecotry });
+  // res.send('Landing Page')
+})
 
 app.use('/songs', songsRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile('public/index.html' , { root : __dirname});
-});
-
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-});
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT}`));
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,37 +1,12 @@
 import React from 'react';
-import axios from 'axios';
 import './style.css';
 
-export default class App extends React.Component{
-  state = {
-    songs: [],
-  };
-  componentDidMount() {
-    axios.get("./data/songs.json").then((response) => {
-      this.setState({songs: response.data});
-    });
-  }
-  render() {
-  const { songs } = this.state;
+function App() {
   return (
-    <div>
-      <ul className="songs">
-          {songs.map((song) => (
-            <li className="song">
-              <p>
-                <strong>Title:</strong> {song.title}
-              </p>
-              <p>
-                <strong>Artist:</strong> {song.artist}
-              </p>
-              <p>
-                <strong>Year:</strong> {song.year}
-              </p>
-            </li>
-          ))}
-        </ul>
+    <div className="App">
+      <h1>App Component</h1>
     </div>
   );
-  }
 }
 
+export default App;
